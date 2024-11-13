261f84702a3ae3234603b0f4a5b990a8e9858c7b
mohammad <mohammad@gmail.com>
2024-11-13 10:44:26 +0330

    some files added

--- DB/panjom/tarhedars/index.js
+++ DB/panjom/tarhedars/index.js
@@ -13,7 +13,49 @@ const data = [
         image:"/images/components/modules/books/panjom/riazi.jpg",
         slug: "/panjom/tarhedars/50021"
     } , 
-    
+    {
+        id:50022,
+        name:"طرح درس سالانه ریاضی پنجم ابتدایی",
+        caption:"طرح درس سالانه ریاضی پنجم ابتدایی - پایه پنجم دبستان",
+        creator:"محمد لبافی",
+        upoledDate:"1403/08/23",
+        fileType:"WORD",
+        src:"/Files/panjom/tarhedars/طرح درس سالانه ریاضی پنجم ابتدایی.doc",
+        book:"riazi",
+        boolInP:"ریاضی",
+        gradeInP:"پایه پنجم",
+        image:"/images/components/modules/books/panjom/riazi.jpg",
+        slug: "/panjom/tarhedars/50022"
+    } ,
+    {
+        id:50023,
+        name:"طرح درس سالانه بخوانیم پنجم ابتدایی",
+        caption:"طرح درس سالانه بخوانیم پنجم ابتدایی - پایه پنجم",
+        creator:"محمد لبافی",
+        upoledDate:"1403/08/23",
+        fileType:"WORD",
+        src:"/Files/panjom/tarhedars/طرح درس سالانه بخوانیم پنجم ابتدایی.doc",
+        book:"farsi",
+        boolInP:"فارسی",
+        gradeInP:"پایه پنجم",
+        image:"/images/components/modules/books/panjom/farsi.jpg",
+        slug: "/panjom/tarhedars/50023"
+    } , 
+    {
+        id:50024,
+        name:"طرح درس روزانه جمعیت ایران مطالعات اجتماعی",
+        caption:"طرح درس جمعیت ایران مطالعات اجتماعی پنجم ابتدایی",
+        creator:"محمد لبافی",
+        upoledDate:"1403/08/23",
+        fileType:"WORD",
+        src:"/Files/panjom/tarhedars/طرح درس جمعیت ایران مطالعات اجتماعی پنجم ابتدایی.doc",
+        book:"motaleat",
+        boolInP:"مطالعات اجتماعی",
+        gradeInP:"پایه پنجم",
+        image:"/images/components/modules/books/panjom/motaleat.jpg",
+        slug: "/panjom/tarhedars/50024"
+    } , 
+        
 
 ]
 
