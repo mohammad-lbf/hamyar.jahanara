372910bdac0149aacf7397ed6ae74fd917a21619
mohammad <mohammad@gmail.com>
2024-12-29 22:44:31 +0330

    new items added - teachers images updated

--- pages/sheshom/soalat/index.js
+++ pages/sheshom/soalat/index.js
@@ -170,7 +170,7 @@ const Soalat = () => {
                 >
                   <option value="all">همه طراحان ({data.length})</option>
                   <option value="محمد لبافی">محمد لبافی ({countByCreator["محمد لبافی"] || 0})</option>
-                  {/* <option value="خانم ندا باقریان">خانم ندا باقریان ({countByCreator["خانم ندا باقریان"] || 0})</option> */}
+                  <option value="امیر عدلی یکتا">امیر عدلی یکتا ({countByCreator["امیر عدلی یکتا"] || 0})</option>
                   {/* <option value="خانم مریم سلطانی">خانم مریم سلطانی ({countByCreator["خانم مریم سلطانی"] || 0})</option> */}
                   {/* <option value="سپهر ابراهیم پور">سپهر ابراهیم پور ({countByCreator["سپهر ابراهیم پور"] || 0})</option> */}
                 </select>
